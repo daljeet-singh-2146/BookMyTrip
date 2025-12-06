4024086d1120d9114aa56c754cd5198314dacb5c
daljeet-singh-2146 <daljeetsinghkhalsa1097@gmail.com>
2025-12-06 20:16:31 +0530

    Add files via upload

---
+++ data/buses-data.js
@@ -0,0 +1,38 @@
+export const buses = [
+    {
+        id: '1',
+        source: 'delhi',
+        destination: 'manali',
+        busName: 'Flixbus',
+        price: 1950,
+        departureTime: '23:00',
+        seatEmpty: 24
+    },
+    {
+        id: '2',
+        source: 'delhi',
+        destination: 'manali',
+        busName: 'Zingbus',
+        price: 2050,
+        departureTime: '19:00',
+        seatEmpty: 24
+    },
+    {
+        id: '3',
+        source: 'delhi',
+        destination: 'mumbai',
+        busName: 'Flixbus',
+        price: 7500,
+        departureTime: '18:00',
+        seatEmpty: 24
+    },
+    {
+        id: '4',
+        source: 'delhi',
+        destination: 'mumbai',
+        busName: 'Zingbus',
+        price: 8500,
+        departureTime: '22:00',
+        seatEmpty: 24
+    }
+]
