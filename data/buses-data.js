e8f218d49aa9e05cd3ca82dcb59eca45e85664c2
daljeet-singh-2146 <daljeetsinghkhalsa1097@gmail.com>
2026-03-13 00:49:17 +0530

    Update buses-data.js

--- data/buses-data.js
+++ data/buses-data.js
@@ -34,5 +34,77 @@ export const buses = [
         price: 8500,
         departureTime: '22:00',
         seatEmpty: 24
+    },
+    {
+        id: '5',
+        source: 'delhi',
+        destination: 'hyderabad',
+        busName: 'Flixbus',
+        price: 5500,
+        departureTime: '19:00',
+        seatEmpty: 24
+    },
+    {
+        id: '6',
+        source: 'delhi',
+        destination: 'hyderabad',
+        busName: 'Zingbus',
+        price: 6500,
+        departureTime: '21:00',
+        seatEmpty: 24
+    },
+    {
+        id: '7',
+        source: 'delhi',
+        destination: 'pune',
+        busName: 'Flixbus',
+        price: 4800,
+        departureTime: '20:00',
+        seatEmpty: 24
+    },
+    {
+        id: '8',
+        source: 'delhi',
+        destination: 'pune',
+        busName: 'Zingbus',
+        price: 5800,
+        departureTime: '23:00',
+        seatEmpty: 24
+    },
+    {
+        id: '9',
+        source: 'delhi',
+        destination: 'ahemdabad',
+        busName: 'Flixbus',
+        price: 6000,
+        departureTime: '17:00',
+        seatEmpty: 24
+    },
+    {
+        id: '10',
+        source: 'delhi',
+        destination: 'ahemdabad',
+        busName: 'Zingbus',
+        price: 7000,
+        departureTime: '22:00',
+        seatEmpty: 24
+    },
+    {
+        id: '11',
+        source: 'delhi',
+        destination: 'chennai',
+        busName: 'Flixbus',
+        price: 9500,
+        departureTime: '23:00',
+        seatEmpty: 24
+    },
+    {
+        id: '12',
+        source: 'delhi',
+        destination: 'chennai',
+        busName: 'Zingbus',
+        price: 10500,
+        departureTime: '22:00',
+        seatEmpty: 24
     }
 ]
